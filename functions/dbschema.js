597a3684f64cec578d24605a23554e4e9db8bbd4
biratlamichhane <lcbirat@gmail.com>
2020-04-13 18:05:46 -0500

    checking db update

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is my body",
+      createdAt: "1996-10-15T00:05:32.000Z",
+      likeCount: 4,
+      commentCount: 4,
+    },
+  ],
+};
