05d4e7f36bd145f27db76e062a973daa971577f3
biratlamichhane <lcbirat@gmail.com>
2020-04-19 17:45:36 -0500

    Code Refractoring

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCv1TDYCDCZuF0CUzlT8g07-LftqFQzuYM",
+  authDomain: "socialapp-df35c.firebaseapp.com",
+  databaseURL: "https://socialapp-df35c.firebaseio.com",
+  projectId: "socialapp-df35c",
+  storageBucket: "socialapp-df35c.appspot.com",
+  messagingSenderId: "940761753601",
+  appId: "1:940761753601:web:770785f45af940fc633800",
+  measurementId: "G-FERNGGNZ4Q",
+};
